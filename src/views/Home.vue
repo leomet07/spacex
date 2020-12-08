6bafe63368e1d3e776d06d25a2423ab27df31746
leomet07 <leomet07@gmail.com>
2020-12-07 20:49:46 -0500

    Basic Func

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../../public/img/rocket.jpg" id="display" />
+		<h1>SpaceX, humanities biggest corpoarate success.</h1>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+	components: {
+		// HelloWorld,
+	},
+};
 </script>
+<style scoped>
+#display {
+	width: 100px;
+}
+</style>
