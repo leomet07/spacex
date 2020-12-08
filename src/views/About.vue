a58da4140d235208ef882115e29edd14e894d1b3
leomet07 <leomet07@gmail.com>
2020-12-07 22:58:13 -0500

    Added Self Promo

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,5 +4,6 @@
 			This non monitizable project is for a school task, with no
 			sponsorship from SpaceX
 		</h2>
+		<h3><a href="https://github.com/leomet07/spacex">Source Code</a></h3>
 	</div>
 </template>
