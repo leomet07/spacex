ed150414d5cdc2dca67b3e41e6490b9b41cc47ea
leomet07 <leomet07@gmail.com>
2020-12-29 16:59:47 -0500

    Added merlin engine page info

---
+++ src/views/Merlin.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<h1>Merlin Engine</h1>
+		<div id="info">
+			<h3>
+				The SpaceX Merlin is a family of rocket engines developed by
+				SpaceX for use on its Falcon 1, Falcon 9 and Falcon Heavy launch
+				vehicles. Merlin engines use RP-1 and liquid oxygen as rocket
+				propellants in a gas-generator power cycle. The Merlin engine
+				was originally designed for sea recovery and reuse. The injector
+				at the heart of Merlin is of the pintle type that was first used
+				in the Apollo Lunar Module landing engine (LMDE). Propellants
+				are fed via a single shaft, dual impeller turbopump. The
+				turbopump also provides high-pressure fluid for the hydraulic
+				actuators, which then recycles into the low-pressure inlet. This
+				eliminates the need for a separate hydraulic drive system and
+				means that thrust vectoring control failure by running out of
+				hydraulic fluid is not possible.
+			</h3>
+			<img
+				id="merlinenginefiring"
+				src="../../public/img/merlinenginefire.gif"
+				alt="Merlin engine firing"
+			/>
+		</div>
+	</div>
+</template>
+<style scoped>
+#info h3 {
+	text-align: left;
+
+	margin-top: 10px;
+	margin-left: 5%;
+	margin-right: 5%;
+}
+
+#merlinenginefiring {
+	margin-top: 10px;
+	width: 85%;
+	max-width: 600px;
+}
+</style>
