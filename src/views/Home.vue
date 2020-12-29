65a927ab67ab8c7475e6f33d4e371781c067b214
leomet07 <leomet07@gmail.com>
2020-12-29 17:12:18 -0500

    Added font + merlin page update + source

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 		<!-- <img alt="Vue logo" src="../../public/img/rocket.jpg" id="display" /> -->
 		<!-- <img alt="Vue logo" src="../../public/img/landing.gif" id="display" /> -->
 		<h1>SpaceX, humanities biggest corporate success.</h1>
-		<video autoplay muted loop id="mainlandervideo">
+		<video autoplay muted loop class="responsive" id="mainlandervideo">
 			<source src="../../public/img/land.mp4" type="video/mp4" />
 		</video>
 
@@ -12,13 +12,13 @@
 		<img
 			alt="Falcon 9 Takeoff"
 			src="../../public/img/falcon9launch.gif"
-			class="display"
+			class="display responsive"
 		/>
 		<h2>SpaceX has failed many times before aswell...</h2>
 		<img
 			alt="Exploding Image 1"
 			src="../../public/img/explode1.gif"
-			class="display"
+			class="display responsive"
 		/>
 	</div>
 </template>
@@ -44,12 +44,14 @@ h2 {
 	margin-bottom: 5px;
 	font-family: "BankGothic";
 }
+
+.responsive {
+	width: 95%;
+	max-width: 680px;
+}
 #mainlandervideo {
 	margin-top: 10px;
-	width: 75%;
+
 	margin-bottom: 15px;
-}
-.display {
-	width: 75%;
 }
 </style>
--- src/views/Merlin.vue
+++ src/views/Merlin.vue
@@ -22,14 +22,49 @@
 				src="../../public/img/merlinenginefire.gif"
 				alt="Merlin engine firing"
 			/>
+			<h3>
+				As of August 2011, SpaceX was producing Merlin engines at the
+				rate of eight per month, planning eventually to raise production
+				to about 33 engines per month (or 400 per year). By September
+				2013, SpaceX total manufacturing space had increased to nearly
+				93,000 square meters (1,000,000 sq ft) and the factory had been
+				configured to achieve a maximum production rate of up to 40
+				rocket cores per year, enough to use the 400 annual engines
+				envisioned by the earlier engine plan. By October 2014, SpaceX
+				announced it had manufactured the 100th Merlin 1D engine and
+				that engines were now being produced at a rate of 4 per week,
+				soon to be increased to 5. By June 2015, SpaceX was producing
+				Merlin engines at the rate of four Merlin 1D engines per week,
+				with a total production capacity in the factory of a maximum of
+				five per week. In February 2016, SpaceX indicated that the
+				company will need to build hundreds of engines a year in order
+				to support a Falcon 9/Falcon Heavy build rate of 30 rocket cores
+				per year by the end of 2016.
+			</h3>
+			<h4>
+				Source :
+				<a
+					href="https://en.wikipedia.org/wiki/SpaceX_Merlin"
+					target="_blank"
+					>https://en.wikipedia.org/wiki/SpaceX_Merlin</a
+				>
+			</h4>
 		</div>
 	</div>
 </template>
 <style scoped>
+#info {
+	padding-bottom: 60px;
+	font-family: "Roboto", sans-serif;
+}
+
+#info a {
+	color: black;
+}
 #info h3 {
 	text-align: left;
-
 	margin-top: 10px;
+	margin-bottom: 10px;
 	margin-left: 5%;
 	margin-right: 5%;
 }
